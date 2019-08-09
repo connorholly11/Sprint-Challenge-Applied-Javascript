c822bba98a50cf307964fcdcd72dee0d5ce94d54
Connor Holly <connorholly@Connors-MacBook-Pro-2.local>
2019-08-09 09:39:24 -0600

    almost done with header, created outlines and solid start points for tabs and cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+
+
+const article = document.querySelector('.cards-container')
+
+function createArticles(object){
+  //creating elements
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const img = document.createElement('img');
+  //imgContainer closes after img
+  const span = document.createElement('span');
+
+  //creating structure
+  card.appendChild(headline);
+  headline.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(img);
+  //imgContainer closes after img
+  author.appendChild(span);
+
+  //creating class names
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
+  img.classList.add('img');
+  span.classList.add('span');
+
+  //creating text content... going to be getting this from the GET function and using object.(whatever it says in your data from GET function)
+
+
+  return card;
+}
+
+article.appendChild(createArticles())
+
+
+
+
+// axios.get('https://lambda-times-backend.herokuapp.com/articles')
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,55 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+// Might have to mess with the GET request to iterate over the topics
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then((response) => {
+  const data = response.data;
+  console.log(data);
+  console.log(data.topics);
+  const TopicsFunction = createTabs(data);
+  topics.appendChild(TopicsFunction);
+})
+
+// //then create a component based on the data and put in on the .topics 
+
+// const topics = document.querySelector('.topics')
+
+// function createTabs(object){
+//   //creating elements
+//   const tab = document.createElement('div');
+
+//   //creating structure (nothing to append to?)
+
+//   //creating class names
+//   tab.classList.add('tabs');
+
+//   //creating text content
+//   tab.textContent = object.topics;
+
+//   return tab;
+// }
+
+// topics.appendChild(createTabs())
+
+
+
+
+// menuItems.forEach(menuItems => {
+//     const LI = document.createElement('li');
+//     LI.textContent = menuItems;
+//     UL1.appendChild(LI);
+//   });
+
+// followersArray.forEach(items => {
+//     axios.get(`https://api.github.com/users/${items}`)
+//     .then((response) => {
+//       const info = response.data;
+//       const newCard = createCards(info);
+//       // info.appendChild(newCard);
+//       // const = document.querySelector('.cards');
+//       cards.appendChild(newCard);
+//     })
