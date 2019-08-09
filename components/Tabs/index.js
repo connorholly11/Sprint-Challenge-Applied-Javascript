cb0a620a7dc48bf5ed105b598a8e5d38d10af2f7
Connor Holly <connorholly@Connors-MacBook-Pro-2.local>
2019-08-09 12:12:00 -0600

    got rid of extra comments

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -11,36 +11,7 @@
 
 // Might have to mess with the GET request to iterate over the topics
 
-// axios.get('https://lambda-times-backend.herokuapp.com/topics')
-// .then((response) => {
-//   const data = response.data;
-//   const topicsData = data.topics;
-//   console.log(data);
-//   console.log(topicsData);
-//   for (topicsData){
-//     data[topicsData].forEach(
-//     const TopicsFunction = createTabs(data);
-//     items.appendChild(TopicsFunction);)
-//   }
-// //   topicsData.forEach(items => {
-// //     const TopicsFunction = createTabs(data);
-// //     items.appendChild(TopicsFunction);
-// //   })
-  
-// })
 
-// var data = response.data.objects;
-
-//         for (var i = 0; i < data.length; i++) {
-//             let type = data[i].content_type.toLowerCase();
-
-//             if (type !== "episode") {
-//                 isSection(data[i]);
-//             }
-
-//             if (type === "episode") {
-//                 isEpisode(data[i].content_url);
-//             }
 
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
 .then((response) => {
@@ -49,18 +20,7 @@ axios.get('https://lambda-times-backend.herokuapp.com/topics')
     })
 })
 
-// followersArray.forEach(items 
 
-// axios
-//   .get("/api/endpoint")
-//   .then(response => {
-//     this.data = response.data;
-//     this.data.forEach(function(item) {
-//       console.log("found: ", item)
-//       console.log("found id: ", item.id)
-//       this.GetLikes(item.id);
-//     });
-//   })
 
 //then create a component based on the data and put in on the .topics 
 
@@ -75,49 +35,10 @@ function createTabs(object){
   //creating class names
   tab.classList.add('tab');
 
-//   const arraySparse = [1,3,,7];
-//   let numCallbackRuns = 0;
-  
-//   arraySparse.forEach(function(element){
-//     console.log(element);
-//     numCallbackRuns++;
-//   });
-
-  //creating text content
-// object.topics.forEach(function(element){
-//     const div = document.createElement('div');
-//     console.log(element);
-//     tab.appendChild(div);
-// })
+//creating text content
   tab.textContent = object;
 
 
   return tab;
 }
 
-// topicsContainer.appendChild(createTabs())
-
-
-
-// object.topics.forEach(object.topics => {
-//     const individualTab = document.createElement('div');
-//     individualTab.textContent = object.topics;
-//     tab.appendChild(individualTab);
-// })
-
-
-// menuItems.forEach(menuItems => {
-//     const LI = document.createElement('li');
-//     LI.textContent = menuItems;
-//     UL1.appendChild(LI);
-//   });
-
-// followersArray.forEach(items => {
-//     axios.get(`https://api.github.com/users/${items}`)
-//     .then((response) => {
-//       const info = response.data;
-//       const newCard = createCards(info);
-//       // info.appendChild(newCard);
-//       // const = document.querySelector('.cards');
-//       cards.appendChild(newCard);
-//     })
